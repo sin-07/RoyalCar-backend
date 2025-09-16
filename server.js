473e7800e991e3b5be8819c112a136768953bc6b
Aniket Singh <aniket.singh07vs@gmail.com>
2025-09-16 16:31:14 +0530

    solved error of login

--- server.js
+++ server.js
@@ -11,8 +11,14 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true
+  origin: [
+    process.env.CLIENT_URL || 'http://localhost:5173',
+    'https://royal-car-frontend.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
